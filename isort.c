61b364ac43a491645cd785938cd4751d91beee55
Yarin Shapira <yariniii19@gmail.com>
2019-12-17 17:55:11 +0200

    fixing main

--- isort.c
+++ isort.c
@@ -1,4 +1,4 @@
-shift_element(int* arr, int i){
+void shift_element(int* arr, int i){
 
     int temp = *(arr+i);
     
@@ -10,7 +10,7 @@ shift_element(int* arr, int i){
 }
 
 
-insertion_sort (int* arr , int len){
+void insertion_sort (int* arr , int len){
     for (int i = 0; i < len; i++){
         if(*(arr+i) > *(arr+i+1)){
             shift_element(arr, i+1);
--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 #include "isort.c"
 
 #define len 50
+#define length 49
 
 
 int main(){
+    int num = len;
     int arr[len] = {0};
-    int temp = 0;
-    printf("enter a number: ");
-    scanf("%d", arr);
     for (size_t i = 0; i < len; i++){
         scanf("%d", arr + i);
     }
+    void insertion_sort(arr, num);
     for (size_t i = 0; i < len; i++){
-        printf("%d, ", *(arr+i));
+        if (i == length){
+            printf("%d\n", *(arr+i)); 
+        }
+        else{
+            printf("%d,", *(arr+i));
+        }
     }
     return 0;
 }
